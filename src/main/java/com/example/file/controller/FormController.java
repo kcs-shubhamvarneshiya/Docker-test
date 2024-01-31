5e91ad4c7aab3be4176929dc4eee61c65da18349
Shubham Varneshiya <shubham.varneshiya@kcsitglobal.com>
2024-01-31 10:21:52 +0530

    include test route

--- src/main/java/com/example/file/controller/FormController.java
+++ src/main/java/com/example/file/controller/FormController.java
@@ -1,17 +1,14 @@
 package com.example.file.controller;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
-
 @RestController
 public class FormController {
-	
+
 	@GetMapping("/")
 	public String generateForm() throws Exception {
-		 return "Application is running...";
+		return "Hello semicolon..";
 	}
 }
